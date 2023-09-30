e5a3d14060afa68cff463a6998fca630af9add86
Iran Braga <iranbragasf@gmail.com>
2023-09-30 16:14:23 -0300

    change signs in gitsigns

--- nvim/.config/nvim/lua/iranbragasf/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/iranbragasf/plugins/gitsigns.lua
@@ -2,10 +2,9 @@ require("gitsigns").setup({
     signs = {
         add          = { text = "▋" },
         change       = { text = "▋" },
-        delete       = { text = "▋" },
-        topdelete    = { text = "▋" },
+        delete = { text = "󰐊" },
+        topdelete = { text = "󰐊" },
         changedelete = { text = "▋" },
-        untracked    = { text = "▋" },
     },
     current_line_blame = true,
     current_line_blame_opts = { delay = 250 },
