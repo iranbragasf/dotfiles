b141c385cba45b76ba2140e8b4e5b6d27bfb3daf
Iran Braga <iranbragasf@gmail.com>
2025-05-25 00:24:22 -0300

    symlink dotfiles without stow

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # iranbragasf's Dotfiles
 
-To set up these dotfiles, clone this repo and symlink the files using [GNU Stow](https://www.gnu.org/software/stow)
+To set up these dotfiles, clone this repo and symlink the directories.
 
 ```bash
-$ git clone git@github.com:iranbragasf/dotfiles.git
-$ cd dotfiles
-$ stow -v --target=$HOME nvim
+git clone git@github.com:iranbragasf/dotfiles.git
+cd dotfiles
+chmod +x ./scripts/symlink-dotfiles.sh
+./scripts/symlink-dotfiles.sh
 ```
---
+++ scripts/symlink-dotfiles.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+main() {
+    local IGNORE_LIST=(".git" "scripts" ".gitignore" "README.md")
+    local SOURCE_DIR="$HOME/personal/dotfiles"
+    local TARGET_DIR="$XDG_CONFIG_HOME"
+
+    for dir in "$SOURCE_DIR"/*/; do
+        local dirname=$(basename "$dir")
+        local linkpath="$TARGET_DIR/$dirname"
+
+        if [[ " ${IGNORE_LIST[*]} " =~ " $dirname " ]]; then
+            continue
+        fi
+
+        if [[ -L "$linkpath" || -e "$linkpath" ]]; then
+            echo "⚠️  Skipping '$linkpath' (already exists)"
+            continue
+        fi
+
+        ln -s "$dir" "$linkpath"
+        echo "✅ Linked '$linkpath' -> '$dir'"
+    done
+}
+
+main
