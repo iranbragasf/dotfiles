1e517e0e9f4d73f8bf6f816bc4e221b38d3932ff
Iran Braga <iranbragasf@gmail.com>
2025-05-28 15:49:34 -0300

    hide mounted volumes and devices from the dock

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -177,6 +177,7 @@ setup_gnome() {
     gsettings set org.gnome.shell.extensions.ding show-home false
     gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
     gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 4112
+    gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
 
     gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/']"
 
