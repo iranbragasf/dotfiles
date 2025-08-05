cc015a0f2c8ef432fb536d9792520a56dc6a2117
Iran Braga <iranbragasf@gmail.com>
2025-08-05 11:42:07 -0300

    neovim: configure built-in formatting

--- nvim/lua/iranbragasf/plugins/blink.lua
+++ nvim/lua/iranbragasf/plugins/blink.lua
@@ -1,18 +1,18 @@
 return {
     "saghen/blink.cmp",
-    event = 'VimEnter',
-    version = '1.*',
+    event = "VimEnter",
+    version = "1.*",
     dependencies = {
-        'L3MON4D3/LuaSnip',
-        'disrupted/blink-cmp-conventional-commits',
+        "L3MON4D3/LuaSnip",
+        "disrupted/blink-cmp-conventional-commits",
         "bydlw98/blink-cmp-sshconfig",
     },
     --- @module 'blink.cmp'
     --- @type blink.cmp.Config
     opts = {
         keymap = {
-            ['<C-s>'] = { 'show_signature', 'hide_signature', 'fallback' },
-            ['<C-k>'] = false,
+            ["<C-s>"] = { "show_signature", "hide_signature", "fallback" },
+            ["<C-k>"] = false,
         },
         sources = {
             default = {
@@ -22,36 +22,36 @@ return {
                 "buffer",
                 "lazydev",
                 "conventional_commits",
-                "sshconfig"
+                "sshconfig",
             },
             providers = {
                 lazydev = {
                     module = "lazydev.integrations.blink",
                     score_offset = 100,
                     enabled = function()
-                        return vim.opt_local.filetype:get() == 'lua'
+                        return vim.opt_local.filetype:get() == "lua"
                     end,
                 },
                 conventional_commits = {
-                    module = 'blink-cmp-conventional-commits',
+                    module = "blink-cmp-conventional-commits",
                     enabled = function()
-                        return vim.opt_local.filetype:get() == 'gitcommit'
+                        return vim.opt_local.filetype:get() == "gitcommit"
                     end,
                 },
                 sshconfig = {
                     module = "blink-cmp-sshconfig",
                     enabled = function()
-                        return vim.opt_local.filetype:get() == 'sshconfig'
+                        return vim.opt_local.filetype:get() == "sshconfig"
                     end,
-                }
+                },
             },
         },
         signature = {
             enabled = true,
             window = {
                 show_documentation = true,
-            }
+            },
         },
-        snippets = { preset = 'luasnip' },
+        snippets = { preset = "luasnip" },
     },
 }
--- nvim/lua/iranbragasf/plugins/lspconfig.lua
+++ nvim/lua/iranbragasf/plugins/lspconfig.lua
@@ -2,12 +2,19 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = { "b0o/schemastore.nvim" },
     config = function()
-        vim.keymap.set("n", "[g", function() vim.diagnostic.jump({ count = -1 }) end, { noremap = true })
-        vim.keymap.set("n", "]g", function() vim.diagnostic.jump({ count = 1 }) end, { noremap = true })
+        vim.keymap.set("n", "[g", function()
+            vim.diagnostic.jump({ count = -1 })
+        end, { noremap = true })
+        vim.keymap.set("n", "]g", function()
+            vim.diagnostic.jump({ count = 1 })
+        end, { noremap = true })
         vim.keymap.set("n", "[G", function()
             local diagnostics = vim.diagnostic.get(0)
             if #diagnostics == 0 then
-                vim.notify("No more valid diagnostics to move to", vim.log.levels.WARN)
+                vim.notify(
+                    "No more valid diagnostics to move to",
+                    vim.log.levels.WARN
+                )
                 return
             end
             table.sort(diagnostics, function(a, b)
@@ -22,7 +29,10 @@ return {
         vim.keymap.set("n", "]G", function()
             local diagnostics = vim.diagnostic.get(0)
             if #diagnostics == 0 then
-                vim.notify("No more valid diagnostics to move to", vim.log.levels.WARN)
+                vim.notify(
+                    "No more valid diagnostics to move to",
+                    vim.log.levels.WARN
+                )
                 return
             end
             table.sort(diagnostics, function(a, b)
@@ -56,45 +66,198 @@ return {
         vim.api.nvim_create_autocmd("LspAttach", {
             group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
             callback = function(event)
-                vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, { buffer = event.buf })
-                vim.keymap.set({ "n", "x" }, "<Leader>ac", vim.lsp.buf.code_action,  { buffer = event.buf })
-                vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = event.buf })
-                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = event.buf })
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = event.buf })
-                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = event.buf })
-                vim.keymap.set("n", "<Leader>o", vim.lsp.buf.document_symbol, { buffer = event.buf })
-                vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, { buffer = event.buf })
-                vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, { buffer = event.buf })
+                vim.keymap.set(
+                    "n",
+                    "<Leader>rn",
+                    vim.lsp.buf.rename,
+                    { buffer = event.buf }
+                )
+                vim.keymap.set(
+                    { "n", "x" },
+                    "<Leader>ac",
+                    vim.lsp.buf.code_action,
+                    { buffer = event.buf }
+                )
+                vim.keymap.set(
+                    "n",
+                    "gr",
+                    vim.lsp.buf.references,
+                    { buffer = event.buf }
+                )
+                vim.keymap.set(
+                    "n",
+                    "gi",
+                    vim.lsp.buf.implementation,
+                    { buffer = event.buf }
+                )
+                vim.keymap.set(
+                    "n",
+                    "gd",
+                    vim.lsp.buf.definition,
+                    { buffer = event.buf }
+                )
+                vim.keymap.set(
+                    "n",
+                    "gD",
+                    vim.lsp.buf.declaration,
+                    { buffer = event.buf }
+                )
+                vim.keymap.set(
+                    "n",
+                    "<Leader>o",
+                    vim.lsp.buf.document_symbol,
+                    { buffer = event.buf }
+                )
+                vim.keymap.set(
+                    "n",
+                    "gy",
+                    vim.lsp.buf.type_definition,
+                    { buffer = event.buf }
+                )
+                vim.keymap.set(
+                    "n",
+                    "<C-s>",
+                    vim.lsp.buf.signature_help,
+                    { buffer = event.buf }
+                )
 
                 local client = vim.lsp.get_client_by_id(event.data.client_id)
-                if not client then return end
+                if not client then
+                    return
+                end
 
-                if client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-                    local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
-                    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-                        buffer = event.buf,
-                        group = highlight_augroup,
-                        callback = vim.lsp.buf.document_highlight,
-                    })
-                    vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-                        buffer = event.buf,
-                        group = highlight_augroup,
-                        callback = vim.lsp.buf.clear_references,
-                    })
+                if
+                    client:supports_method(
+                        vim.lsp.protocol.Methods.textDocument_documentHighlight,
+                        event.buf
+                    )
+                then
+                    local highlight_augroup = vim.api.nvim_create_augroup(
+                        "lsp-highlight",
+                        { clear = false }
+                    )
+                    vim.api.nvim_create_autocmd(
+                        { "CursorHold", "CursorHoldI" },
+                        {
+                            buffer = event.buf,
+                            group = highlight_augroup,
+                            callback = vim.lsp.buf.document_highlight,
+                        }
+                    )
+                    vim.api.nvim_create_autocmd(
+                        { "CursorMoved", "CursorMovedI" },
+                        {
+                            buffer = event.buf,
+                            group = highlight_augroup,
+                            callback = vim.lsp.buf.clear_references,
+                        }
+                    )
                     vim.api.nvim_create_autocmd("LspDetach", {
-                        group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
+                        group = vim.api.nvim_create_augroup(
+                            "lsp-detach",
+                            { clear = true }
+                        ),
                         callback = function(event2)
                             vim.lsp.buf.clear_references()
-                            vim.api.nvim_clear_autocmds { group = "lsp-highlight", buffer = event2.buf }
+                            vim.api.nvim_clear_autocmds({
+                                group = "lsp-highlight",
+                                buffer = event2.buf,
+                            })
                         end,
                     })
                 end
 
-                -- if client:supports_method('textDocument/completion') then
-                --     vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
-                --     vim.keymap.set("i", "<C-Space>", vim.lsp.completion.get, { buffer = event.buf })
+                -- if client:supports_method("textDocument/completion") then
+                --     -- NOTE: optionally trigger autocompletion on EVERY keypress. May be slow!
+                --     local chars = {}
+                --     for i = 32, 126 do
+                --         table.insert(chars, string.char(i))
+                --     end
+                --     client.server_capabilities.completionProvider.triggerCharacters =
+                --         chars
+                --
+                --     vim.lsp.completion.enable(
+                --         true,
+                --         client.id,
+                --         event.buf,
+                --         { autotrigger = true }
+                --     )
+                --     vim.keymap.set(
+                --         "i",
+                --         "<C-Space>",
+                --         vim.lsp.completion.get,
+                --         { buffer = event.buf }
+                --     )
+                -- end
+
+                -- if
+                --     not client:supports_method("textDocument/willSaveWaitUntil")
+                --     and client:supports_method("textDocument/formatting")
+                -- then
+                --     local default_format_opts = {
+                --         bufnr = event.buf,
+                --         id = client.id,
+                --         timeout_ms = 500,
+                --     }
+                --     local desc = "Format Document"
+                --
+                --     vim.api.nvim_create_autocmd("BufWritePre", {
+                --         group = vim.api.nvim_create_augroup(
+                --             "lsp-formatting",
+                --             { clear = false }
+                --         ),
+                --         buffer = event.buf,
+                --         callback = function()
+                --             if vim.g.format_on_save then
+                --                 vim.lsp.buf.format(default_format_opts)
+                --             end
+                --         end,
+                --         desc = desc,
+                --     })
+                --
+                --     vim.keymap.set({ "n", "v" }, "<M-f>", function()
+                --         vim.lsp.buf.format(default_format_opts)
+                --         local mode = vim.api.nvim_get_mode().mode
+                --         -- NOTE: leave visual mode after range format.
+                --         if vim.startswith(string.lower(mode), "v") then
+                --             vim.api.nvim_feedkeys(
+                --                 vim.api.nvim_replace_termcodes(
+                --                     "<Esc>",
+                --                     true,
+                --                     false,
+                --                     true
+                --                 ),
+                --                 "n",
+                --                 true
+                --             )
+                --         end
+                --     end, {
+                --         noremap = true,
+                --         desc = desc,
+                --     })
+                --
+                --     vim.api.nvim_create_user_command("Format", function(args)
+                --         local range = nil
+                --         if args.count ~= -1 then
+                --             local end_line = vim.api.nvim_buf_get_lines(
+                --                 0,
+                --                 args.line2 - 1,
+                --                 args.line2,
+                --                 true
+                --             )[1]
+                --             range = {
+                --                 start = { args.line1, 0 },
+                --                 ["end"] = { args.line2, end_line:len() },
+                --             }
+                --         end
+                --         vim.lsp.buf.format({ range = range })
+                --     end, {
+                --         nargs = 0,
+                --         range = true,
+                --         desc = desc,
+                --     })
                 -- end
             end,
         })
-    end
+    end,
 }
