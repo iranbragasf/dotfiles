f6917b171939cd51e755cbc571c020014857a4b0
Iran Braga <iranbragasf@gmail.com>
2025-08-05 21:33:28 -0300

    neovim: set colorscheme to onedark

--- nvim/lua/iranbragasf/plugins/onedark.lua
+++ nvim/lua/iranbragasf/plugins/onedark.lua
@@ -1,11 +1,14 @@
--- return {
---   "navarasu/onedark.nvim",
---   priority = 1000, -- make sure to load this before all the other start plugins
---   config = function()
---     require('onedark').setup {
---       style = 'darker'
---     }
---     -- Enable theme
---     require('onedark').load()
---   end
--- }
+return {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- NOTE: ensure it loads first.
+    config = function()
+        require("onedarkpro").setup({
+            options = {
+                cursorline = true,
+                transparency = false,
+                highlight_inactive_windows = true,
+            },
+        })
+        vim.cmd("colorscheme onedark")
+    end,
+}
