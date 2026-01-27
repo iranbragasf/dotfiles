d081f23c391c20318bf3646d019ce0fe17a4938f
Iran Braga <iranbragasf@gmail.com>
2026-01-27 09:26:38 -0300

    install anydesk

--- scripts/set-up-ubuntu.sh
+++ scripts/set-up-ubuntu.sh
@@ -182,6 +182,14 @@ EOF
     pipx install tldr
     tldr --print-completion bash >~/.local/share/bash-completion/completions/tldr
 
+    # Install AnyDesk
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY -o /etc/apt/keyrings/keys.anydesk.com.asc
+    sudo chmod a+r /etc/apt/keyrings/keys.anydesk.com.asc
+    echo "deb [signed-by=/etc/apt/keyrings/keys.anydesk.com.asc] https://deb.anydesk.com all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list >/dev/null
+    sudo apt update
+    sudo apt install -y apt-transport-https anydesk
+
     set_up_flatpak
     install_flatpaks
 }
