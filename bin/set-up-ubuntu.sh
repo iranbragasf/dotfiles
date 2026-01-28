08213b5611b0f76000cc30b96f3c757cdf5a9be7
Iran Braga <iranbragasf@gmail.com>
2026-01-28 16:12:42 -0300

    activate mise shims

--- bin/set-up-ubuntu.sh
+++ bin/set-up-ubuntu.sh
@@ -80,6 +80,7 @@ install_mise_tools() {
     for tool in "${tools[@]}"; do
         mise use -g "${tool}"
     done
+    eval "$(mise activate bash --shims)"
     mkdir -vp ~/.local/share/bash-completion/completions/
     mise completion bash --include-bash-completion-lib >~/.local/share/bash-completion/completions/mise
     pipx ensurepath
@@ -159,8 +160,8 @@ install_packages() {
     # Install GitHub CLI
     sudo mkdir -p -m 755 /etc/apt/keyrings
     out=$(mktemp)
-    wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg
-    cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null
+    wget -nv -O"$out" https://cli.github.com/packages/githubcli-archive-keyring.gpg
+    cat "$out" | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null
     sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
     sudo apt update
